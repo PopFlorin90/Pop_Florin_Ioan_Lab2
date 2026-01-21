5d5551cf445e46fc2d852d4532335830a58bed64
Florin Ioan Pop <pop.florin90@gmail.com>
2026-01-21 21:07:54 +0200

    Add project files.

---
+++ Data/Pop_Florin_Ioan_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Florin_Ioan_Lab2.Models;
+
+namespace Pop_Florin_Ioan_Lab2.Data
+{
+    public class Pop_Florin_Ioan_Lab2Context : DbContext
+    {
+        public Pop_Florin_Ioan_Lab2Context (DbContextOptions<Pop_Florin_Ioan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Florin_Ioan_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Pop_Florin_Ioan_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,10 @@
+﻿namespace Pop_Florin_Ioan_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+        public string PublisherName { get; set; }
+        public ICollection<Book>? Books { get; set; } //navigation property
+        public string PublisherAddress { get; set; }
+    }
+}
